b5886a3046b7b5da5e2e8ae3266255a59b6f1eb5
rainbrry <ade.usman403@gmail.com>
2022-11-05 10:29:34 +0700

    change index

--- src/views/pages/home/index.js
+++ src/views/pages/home/index.js
@@ -4,21 +4,6 @@ export default function Home() {
 	return (
 		<div className="w-full p-4 bg-cyan-400 text-white">
 			<h3>Hello</h3>
-
-			<div className="p-10">
-				<button
-					type="submit"
-					className="bg-blue-500 text-white px-4 py-2 rounded shadow-lg"
-				>
-					Login
-				</button>
-				<button
-					type="submit"
-					className="bg-red-500 text-white px-4 py-2 rounded shadow-lg"
-				>
-					Logout
-				</button>
-			</div>
 		</div>
 	);
 }
