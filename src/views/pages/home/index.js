6c422a076bd27ebede194d19d4ccef3ddd83cd1a
rainbrry <ade.usman403@gmail.com>
2022-11-05 08:16:37 +0700

    change home/index

--- src/views/pages/home/index.js
+++ src/views/pages/home/index.js
@@ -1,6 +1,17 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 export default function Home() {
+	useEffect(() => {
+		async function fetchData() {
+			await axios
+				.get("http://localhost:5000/users")
+				.then((res) => console.log(res.data));
+		}
+
+		fetchData();
+	}, []);
+
 	return (
 		<div className="w-full p-4 bg-cyan-400 text-white">
 			<h3>Hello</h3>
